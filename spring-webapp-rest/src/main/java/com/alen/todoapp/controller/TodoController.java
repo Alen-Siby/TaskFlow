137867dadcd2c64b8625cf4ba19eaf8d179f99fd
darkwizard3801 <alensiby84@gmail.com>
2025-06-24 13:24:49 +0530

    changed the controller

--- spring-webapp-rest/src/main/java/com/alen/todoapp/controller/TodoController.java
+++ spring-webapp-rest/src/main/java/com/alen/todoapp/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
+import java.util.List;
 
 @RequestMapping("/todo")
 @RestController
@@ -25,38 +26,38 @@ public class TodoController {
     }
 
     @GetMapping()
-    public ResponseEntity<?> getAllTodos() {
+    public ResponseEntity<List<TodoDto>> getAllTodos() {
         return ResponseEntity.ok(todoservice.getAllTodos());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getTodo(@PathVariable int id) {
+    public ResponseEntity<TodoDto> getTodo(@PathVariable int id) {
         TodoDto todoDto = todoservice.getTodoById(id);
         return new ResponseEntity<>(todoDto, HttpStatus.OK);
     }
 
     @PostMapping()
-    public ResponseEntity<?> addTodo(@RequestBody @Valid TodoDto todoDto) {
+    public ResponseEntity<TodoDto> addTodo(@RequestBody @Valid TodoDto todoDto) {
 
         TodoDto createdTodo = todoservice.addTodo(todoDto);
         return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateTodo(@PathVariable int id, @RequestBody @Valid TodoDto todoDto) {
+    public ResponseEntity<TodoDto> updateTodo(@PathVariable int id, @RequestBody @Valid TodoDto todoDto) {
 
         TodoDto updated = todoservice.updateTodo(id, todoDto);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTodo(@PathVariable int id) {
+    public ResponseEntity<TodoDto> deleteTodo(@PathVariable int id) {
         todoservice.deleteTodo(id);
         return ResponseEntity.noContent().build();
     }
 
 //    @PostMapping("/load")
-//    public ResponseEntity<?> loadData() {
+//    public ResponseEntity<todoDto> loadData() {
 //        service.load();
 //        return ResponseEntity.ok().build();
 //    }
